0e414427577d3de71f44313e0096307123f5e0ba
Alessandro Galdelli <a.galdelli@pm.univpm.it>
2022-05-23 16:49:42 +0200

    Added my project

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
        {
            name: 'Magnemite',
            location: {
-               lat: 44.496470,
-               lng: 11.320180,
+               lat: 43.398181,
+               lng: 13.052366,
            }
        },
    ];
